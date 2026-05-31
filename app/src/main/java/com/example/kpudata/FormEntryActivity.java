f1774804adf594f12b9671dd10f31f6191aa8b02
hibiidesu <falih.khairullah@gmail.com>
2026-05-31 13:04:59 +0700

    Fixed FormEntryActivity and ViewDataActivity from java to kotlin

--- app/src/main/java/com/example/kpudata/FormEntryActivity.java
+++
@@ -1,4 +0,0 @@
-package com.example.kpudata;
-
-public class FormEntryActivity {
-}
---
+++ app/src/main/java/com/example/kpudata/FormEntryActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kpudata
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.RadioGroup
+import androidx.appcompat.app.AppCompatActivity
+
+public class FormEntryActivity {
+}
